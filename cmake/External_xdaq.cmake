900e3d38238ba1fbf354e7942116fe404f11d27f
Laurent Pétré <lpetre@ulb.ac.be>
2019-03-01 23:30:58 +0100

    Fix installation path of `xdaq-worksuite/extern/oracle`
    Change the installation path from $(XDAQ_ROOT) to $(INSTALL_PREIFX) as
    it is for all the other libraries.

--- cmake/External_xdaq.cmake
+++ cmake/External_xdaq.cmake
@@ -40,9 +40,9 @@ ExternalProject_Add(xdaq-worksuite
   # sources are then built on every run and we do not want that.
   URL https://gitlab.cern.ch/cmsos/worksuite/-/archive/release_14_9_0_0/worksuite-release_14_9_0_0.tar.bz2
 
-  PATCH_COMMAND
-    sed "s/BUILD_HOME/XDAQ_ROOT/g" "${CMAKE_CURRENT_BINARY_DIR}/thirdparty/xdaq/core/src/xdaq-core/mfDefs.core" > "<SOURCE_DIR>/mfDefs.core"
-    && echo "include <SOURCE_DIR>/mfDefs.core" >> "<SOURCE_DIR>/mfDefs.worksuite"
+  PATCH_COMMAND patch -p1 -i "${CMAKE_SOURCE_DIR}/thirdparty/oracle-makefile.patch"
+  COMMAND       sed "s/BUILD_HOME/XDAQ_ROOT/g" "${CMAKE_CURRENT_BINARY_DIR}/thirdparty/xdaq/core/src/xdaq-core/mfDefs.core" > "<SOURCE_DIR>/mfDefs.core"
+  COMMAND       echo "include <SOURCE_DIR>/mfDefs.core" >> "<SOURCE_DIR>/mfDefs.worksuite"
 
   CONFIGURE_COMMAND ""
   BUILD_IN_SOURCE TRUE

e12c0b6f0f9af72c6282648421b9352502fea215
Laurent Pétré <lpetre@ulb.ac.be>
2019-03-17 18:09:35 +0100

    Add recursive dependencies infrastructure

--- cmake/SuperbuildProject.cmake
+++ cmake/SuperbuildProject.cmake
@@ -1,22 +1,56 @@
 include(ExternalProject)
 
-function(superbuild_add_project _name)
-  string(TOLOWER "${_name}" _name)
+function(superbuild_add_project _project_name)
+  # Arguments parsing
+  cmake_parse_arguments(_args "" "TARGET;DEPENDENCY_NAME" "" ${ARGN})
 
-  cmake_parse_arguments(_args "" "GEM_TARGET" "" ${ARGN})
-  string(TOLOWER "${_args_GEM_TARGET}" _gem_target)
-
-  message(STATUS "${_name} requested for the  ${_gem_target} target.")
-
-  set(EP_NAME "${_name}.${_gem_target}")
-  set(EP_PREFIX "${CMAKE_BINARY_DIR}/${EP_NAME}")
-
-  if(EXISTS "${CMAKE_SOURCE_DIR}/projects/${_name}/${_gem_target}.superbuild.cmake")
-    include("${CMAKE_SOURCE_DIR}/projects/${_name}/${_gem_target}.superbuild.cmake")
-  elseif(EXISTS "${CMAKE_SOURCE_DIR}/projects/${_name}/superbuild.cmake")
-    include("${CMAKE_SOURCE_DIR}/projects/${_name}/superbuild.cmake")
-  else()
-    message(FATAL_ERROR "Project not found !")
+  if(_args_TARGET)
+    set(SUPERBUILD_TARGET "${_args_TARGET}")
   endif()
+
+  string(TOLOWER "${_project_name}" _project_name)
+  string(TOLOWER "${SUPERBUILD_TARGET}" _gem_target)
+
+  # Available variables for the EP
+  set(EP_NAME "${_project_name}.${_gem_target}")
+  set(EP_PREFIX "${CMAKE_BINARY_DIR}/${EP_NAME}")
+  set(EP_DEPENDENCIES "")
+
+  # Return EP_NAME if requested
+  if(_args_DEPENDENCY_NAME)
+    set("${_args_DEPENDENCY_NAME}" "${EP_NAME}" PARENT_SCOPE)
+  endif()
+
+  # Include the project only once
+  get_property(included GLOBAL PROPERTY "${EP_NAME}" SET)
+  if(NOT included)
+    set_property(GLOBAL PROPERTY "${EP_NAME}" TRUE)
+
+    set(_build_prefix_dir "${CMAKE_SOURCE_DIR}/projects/${_project_name}/")
+
+    if(EXISTS "${_build_prefix_dir}/${_gem_target}.superbuild.cmake")
+      include("${_build_prefix_dir}/${_gem_target}.superbuild.cmake")
+    elseif(EXISTS "${_build_prefix_dir}/superbuild.cmake")
+      include("${_build_prefix_dir}/superbuild.cmake")
+    else()
+      message(FATAL_ERROR "Project ${_project_name} for ${_gem_target} not found !")
+    endif()
+
+    message(STATUS "${EP_NAME} added !")
+  endif()
+endfunction()
+
+function(superbuild_add_dependencies)
+  # Add all dependencies
+  foreach(_dependency ${ARGN})
+    superbuild_add_project("${_dependency}"
+      DEPENDENCY_NAME _dependency_targetname)
+    if(_dependency_targetname)
+      list(APPEND EP_DEPENDENCIES "${_dependency_targetname}")
+    endif()
+  endforeach()
+
+  set(EP_DEPENDENCIES "${EP_DEPENDENCIES}" PARENT_SCOPE)
+  message(STATUS "${EP_NAME} will depend of ${EP_DEPENDENCIES}")
 endfunction()
 

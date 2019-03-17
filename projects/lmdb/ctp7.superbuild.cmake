f519047e55f84e01d35c281dc7b96388893301e4
Laurent Pétré <lpetre@ulb.ac.be>
2019-03-17 20:20:53 +0100

    Move `LMDB` to the new dependency system

--- projects/lmdb/ctp7.superbuild.cmake
+++ projects/lmdb/ctp7.superbuild.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(${EP_NAME}
   PREFIX "${EP_PREFIX}"
-  INSTALL_DIR "${CMAKE_BINARY_DIR}/install/arm"
+  INSTALL_DIR "${EP_INSTALL_DIR}"
 
   DOWNLOAD_DIR "${GEM_SUPERBUILD_CACHE_DIR}"
   URL "https://github.com/LMDB/lmdb/archive/LMDB_0.9.19.tar.gz"
@@ -8,11 +8,11 @@ ExternalProject_Add(${EP_NAME}
 
   CONFIGURE_COMMAND ""
   BUILD_IN_SOURCE TRUE
-  BUILD_COMMAND make -C ./libraries/liblmdb/ -j8
+  BUILD_COMMAND $(MAKE) -C ./libraries/liblmdb/
     "CC=arm-linux-gnueabihf-gcc"
     "AR=arm-linux-gnueabihf-ar"
     "CFLAGS=-fomit-frame-pointer -pipe -fno-common -fno-builtin -Wall -march=armv7-a -mfpu=neon -mfloat-abi=hard -mthumb-interwork -mtune=cortex-a9 -DEMBED -Dlinux -D__linux__ -Dunix -fPIC --sysroot=${CTP7_SYSROOT} -pthread"
-  INSTALL_COMMAND make -C ./libraries/liblmdb install
+  INSTALL_COMMAND $(MAKE) -C ./libraries/liblmdb install
     "prefix=/usr"
     "DESTDIR=<INSTALL_DIR>"
 )

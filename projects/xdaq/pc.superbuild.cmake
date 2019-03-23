271badf03bea58928b37739b403983ca9640fada
Laurent Pétré <lpetre@ulb.ac.be>
2019-03-23 17:47:39 +0100

    Move to the master branch of `xdaq-core`
    * The master branch is followed with fixes in the Makefiles for
      compilation.
    * Parallel compilation inside each packages is enabled.

--- projects/xdaq/pc.superbuild.cmake
+++ projects/xdaq/pc.superbuild.cmake
@@ -31,18 +31,19 @@ ExternalProject_Add(core.${EP_NAME}
   INSTALL_DIR "${EP_INSTALL_DIR}"
 
   DOWNLOAD_DIR "${SUPERBUILD_CACHE_DIR}"
-  URL https://gitlab.cern.ch/cmsos/core/-/archive/release_14_10_0_0/core-release_14_10_0_0.tar.bz2
-  URL_HASH SHA256=2cb223ded3db28f67443f38dc7b40a432667643fc3f96983402b7f4c3cdafcfa
+  URL https://gitlab.cern.ch/cmsos/core/-/archive/1f55790f611805c3a1e63316669514ab12b737f5/core-1f55790f611805c3a1e63316669514ab12b737f5.tar.bz2
+  URL_HASH SHA256=15a054a25a07ca9aeea8f254c7e2c9366191cf807f4ab1118b83fc0f4f6ceedf
 
-  PATCH_COMMAND patch -p1 -i "${CMAKE_CURRENT_LIST_DIR}/xalan-makefile.patch"
-  COMMAND       patch -p1 -i "${CMAKE_CURRENT_LIST_DIR}/xdaq-core-cpp11.patch"
-  COMMAND       patch -p1 -i "${CMAKE_CURRENT_LIST_DIR}/xdaq-core-asyncresolv.patch"
+  PATCH_COMMAND patch -p1 -i "${CMAKE_CURRENT_LIST_DIR}/core-00-xalan.patch"
+  COMMAND       patch -p1 -i "${CMAKE_CURRENT_LIST_DIR}/core-01-boost_thread.patch"
+  COMMAND       patch -p1 -i "${CMAKE_CURRENT_LIST_DIR}/core-02-asyncresolv.patch"
+  COMMAND       patch -p1 -i "${CMAKE_CURRENT_LIST_DIR}/core-03-parallel_build.patch"
 
   CONFIGURE_COMMAND ""
   BUILD_IN_SOURCE TRUE
-  BUILD_COMMAND make
+  BUILD_COMMAND $(MAKE)
     ${_xdaq_core_options}
-  INSTALL_COMMAND make install
+    INSTALL_COMMAND $(MAKE) install
     ${_xdaq_core_options}
 )
 

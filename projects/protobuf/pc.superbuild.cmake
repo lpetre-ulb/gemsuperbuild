4d13f38b24b1a7f88c203073023e084844a38ef0
Laurent Pétré <lpetre@ulb.ac.be>
2019-03-23 16:19:58 +0100

    Add the `protobuf` project
    This is the first step to compile the superbuild on `lxplus7`.

---
+++ projects/protobuf/pc.superbuild.cmake
@@ -0,0 +1,19 @@
+ExternalProject_Add(${EP_NAME}
+  PREFIX "${EP_PREFIX}"
+  INSTALL_DIR "${EP_INSTALL_DIR}"
+
+  DOWNLOAD_DIR "${SUPERBUILD_CACHE_DIR}"
+  URL https://github.com/protocolbuffers/protobuf/releases/download/v3.7.0/protobuf-cpp-3.7.0.tar.gz
+  URL_HASH SHA256=797e759e6daf644be4c09070d4777d178b538dca310cc4956fde4b51b02ba2d3
+
+  SOURCE_SUBDIR
+    "cmake"
+  CMAKE_ARGS
+    cmake
+    "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>/usr"
+    -DBUILD_SHARED_LIBS=ON
+    -DCMAKE_INSTALL_LIBDIR=lib
+    # FIXME: build tests only when asked by the user
+    #-Dprotobuf_BUILD_TESTS=OFF
+)
+
